06b17f0576a6832d04e18181eca7f367020d3ea0
Oleksandr Kapeliukhovskyi <akapelyukhovsky@ingenio.com>
2024-10-16 16:25:52 -0700

    GP-1535: Prototyping with "Accept a payment on a hosted page" - https://www.checkout.com/docs/payments/accept-payments/accept-a-payment-on-a-hosted-page

--- server.js
+++ server.js
@@ -16,6 +16,58 @@ app.get('/config', (req, res) => {
   });
 });
 
+app.post("/add-card", async (req, res) => {
+  const { token } = req.body;  // Destructuring the token from the request body
+
+  console.log('Token:', token);  // Output the token for debugging
+
+  const request = await fetch(
+    "https://api.sandbox.checkout.com/hosted-payments",
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${SECRET_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        // amount: 0,
+        currency: "USD",
+        source: {
+          type: "card"
+        },
+        billing: {
+          address: {
+            // address_line1: "221 Main street",
+            // city: "San Francisco",
+            state: "CA",
+            zip: "94105",
+            country: "US"
+          },
+          // phone: {
+          //   country_code: "+1",
+          //   number: "415 555 2671"
+          // },
+        },
+        customer: {
+          // "email": "Oleksandr@ingenio.com",
+          "name": "Oleksandr"
+        },
+        processing_channel_id: process.env.PROCESSING_CHANNEL_ID,
+        success_url: "http://localhost:3000?success",
+        cancel_url: "http://localhost:3000?cancel",
+        failure_url: "http://localhost:3000?payments-failure"
+      }),
+    }
+  );
+  const parsedPayload = await request.json();
+  console.log(parsedPayload)
+
+  // Handle the rest of your save-card logic here
+
+  res.send({result: 'Card added successfully', redirect_url: parsedPayload._links.redirect.href});  // Send a response back to the client
+});
+
+
 app.post("/save-card", async (req, res) => {
   const { token } = req.body;  // Destructuring the token from the request body
 
